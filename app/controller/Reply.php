399dba808ded5d2e8bbebd9a010220ff2013dbe0
cqasen <cqasen@qq.com>
2021-12-07 21:53:54 +0800

    122

--- app/controller/Reply.php
+++ app/controller/Reply.php
@@ -3,6 +3,7 @@
 namespace app\controller;
 
 use app\BaseController;
+use think\facade\Log;
 
 class Reply extends BaseController
 {
@@ -11,5 +12,8 @@ class Reply extends BaseController
 		echo "<pre>";
 		$raw_post_data = file_get_contents('php://input', 'r');
 		print_r($raw_post_data);
+		Log::write('Reply', 'notice');
+		Log::write($raw_post_data, 'notice');
+
 	}
 }
