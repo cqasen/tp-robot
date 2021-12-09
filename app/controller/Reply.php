14f46d1e29477947e40b2f3e1bb337cf96159946
cqasen <cqasen@qq.com>
2021-12-09 17:48:47 +0800

    11

--- app/controller/Reply.php
+++ app/controller/Reply.php
@@ -11,59 +11,66 @@ use think\facade\Log;
 
 class Reply extends BaseController
 {
-	public function index()
-	{
+    public function is_json($string)
+    {
+        json_decode($string);
+        return (json_last_error() === JSON_ERROR_NONE);
+    }
+
+    public function index()
+    {
 //		echo "<pre>";
-		$raw_post_data = file_get_contents('php://input', 'r');
+        $raw_post_data = file_get_contents('php://input', 'r');
 //		print_r($raw_post_data);
-		Log::write('Reply', 'notice');
-		Log::write($raw_post_data, 'notice');
-		$params = json_decode($raw_post_data, true) ?? [];
+        Log::write('Reply', 'notice');
+        Log::write($raw_post_data, 'notice');
+
+        $params = $this->is_json($raw_post_data) ? json_decode($raw_post_data, true) : [];
 //
 //		$s      = ' {"conversationId":"cidslUbes2wg1RUOkAM7Ky0BA==","atUsers":[{"dingtalkId":"$:LWCP_v1:$K42eHOm8++kOBHyyBiNVLQqI4W7KQzSV"}],"chatbotCorpId":"ding5b06cf33b246469235c2f4657eb6378f","chatbotUserId":"$:LWCP_v1:$K42eHOm8++kOBHyyBiNVLQqI4W7KQzSV","msgId":"msgmwAH1b2eCUKS+XIORAPtNQ==","senderNick":"森","isAdmin":true,"senderStaffId":"054632473136322716","sessionWebhookExpiredTime":1638974911889,"createAt":1638969511621,"senderCorpId":"ding5b06cf33b246469235c2f4657eb6378f","conversationType":"2","senderId":"$:LWCP_v1:$hnDL6J3jSFO1+VXckD8/zQ==","conversationTitle":"机器人-TEST","isInAtList":true,"sessionWebhook":"https://oapi.dingtalk.com/robot/sendBySession?session=7c22be0369674062ac1f719114c9ced9","text":{"content":"[忍者] "},"robotCode":"dingidpy5p1nj0lknlbq","msgtype":"text"}';
 //		$params = json_decode($s, true);
 //		print_r($params);
-		$dto = ChatbotReplyDto::newInstance($params);
-		Log::write($raw_post_data, 'notice', $dto->toArray());
-		$robotCode = 'dingpekecjsl8bjfiy2u';
-		$userIds[] = $dto->getSenderStaffId();
-		if ($dto->getConversationType() == 2) {
-			//群聊
-			$client   = new Client();
-			$message  = sprintf('你于[%s]发送的消息为：%s', date('Y-m-d H:i:s', $dto->getCreateAt() / 1000),
-				$dto->getText()['content']);
-			$jsonData = [
-				'msgtype' => 'text',
-				'text'    => [
-					'content' => $message,
-				],
-				'at'      => [
-					'atMobiles' => [],
-					'atUserIds' => [
-						$dto->getSenderStaffId(),
-					],
-					'isAtAll'   => false,
-				],
-			];
-			$resp     = $client->post($dto->getSessionWebhook(), ['json' => $jsonData]);
-		} else {
-			//单聊
-			$message = sprintf('你于[%s]发送的消息为：%s', date('Y-m-d H:i:s', $dto->getCreateAt() / 1000),
-				$dto->getText()['content']);
+        $dto = ChatbotReplyDto::newInstance($params);
+        Log::write($raw_post_data, 'notice', $dto->toArray());
+        $robotCode = 'dingpekecjsl8bjfiy2u';
+        $userIds[] = $dto->getSenderStaffId();
+        if ($dto->getConversationType() == 2) {
+            //群聊
+            $client   = new Client();
+            $message  = sprintf('你于[%s]发送的消息为：%s', date('Y-m-d H:i:s', $dto->getCreateAt() / 1000),
+                $dto->getText()['content']);
+            $jsonData = [
+                'msgtype' => 'text',
+                'text'    => [
+                    'content' => $message,
+                ],
+                'at'      => [
+                    'atMobiles' => [],
+                    'atUserIds' => [
+                        $dto->getSenderStaffId(),
+                    ],
+                    'isAtAll'   => false,
+                ],
+            ];
+            $resp     = $client->post($dto->getSessionWebhook(), ['json' => $jsonData]);
+        } else {
+            //单聊
+            $message = sprintf('你于[%s]发送的消息为：%s', date('Y-m-d H:i:s', $dto->getCreateAt() / 1000),
+                $dto->getText()['content']);
 
-			$req = new BatchSendOTORequest();
+            $req = new BatchSendOTORequest();
 
-			$req->robotCode = $robotCode;
-			$req->userIds   = $userIds;    //通过手机号获取userId
-			$req->msgKey    = "officialMarkdownMsg";
-			$msgParam       = [
-				"text"  => $message,
-				"title" => $message,
-			];
-			$req->msgParam  = (string)json_encode($msgParam);
-			DingtalkUtil::newInstance()->batchSend($req);
-		}
-		return $this->responseJson();
+            $req->robotCode = $robotCode;
+            $req->userIds   = $userIds;    //通过手机号获取userId
+            $req->msgKey    = "officialMarkdownMsg";
+            $msgParam       = [
+                "text"  => $message,
+                "title" => $message,
+            ];
+            $req->msgParam  = (string)json_encode($msgParam);
+            DingtalkUtil::newInstance()->batchSend($req);
+        }
+        return $this->responseJson();
 
-	}
+    }
 }
