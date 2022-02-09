fa7728af6a1bf9861ed68ecf89e36a1e8a3bed32
cqasen <cqasen@qq.com>
2022-02-09 17:07:46 +0800

    22

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -52,10 +52,10 @@ class Index extends BaseController
         $req->robotCode = $robotCode;
         $req->userIds   = $userIds;    //通过手机号获取userId
 //        $req->msgKey    = "officialMarkdownMsg";
-        $req->msgKey    = "officialActionCardMsg";
-        $date           = date('Y-m-d H:i:s');
-        $raw_post_data  = file_get_contents('php://input', 'r');
-        $txt            = '';
+        $req->msgKey   = "sampleActionCard";
+        $date          = date('Y-m-d H:i:s');
+        $raw_post_data = file_get_contents('php://input', 'r');
+        $txt           = '';
         foreach ($_SERVER as $key => $value) {
             $txt .= sprintf("> %s: %s \n\n", $key, $value);
         }
@@ -67,22 +67,10 @@ class Index extends BaseController
 
         $jsonStr = <<<EOF
 {
-    "msgtype": "actionCard",
-    "actionCard": {
-        "title": "乔布斯 20 年前想打造一间苹果咖啡厅，而它正是 Apple Store 的前身", 
-        "text": "![screenshot](https://img.alicdn.com/tfs/TB1NwmBEL9TBuNjy1zbXXXpepXa-2400-1218.png) \n\n #### 乔布斯 20 年前想打造的苹果咖啡厅 \n\n Apple Store 的设计正从原来满满的科技感走向生活化，而其生活化的走向其实可以追溯到 20 年前苹果一个建立咖啡馆的计划", 
-        "btnOrientation": "0", 
-        "btns": [
-            {
-                "title": "内容不错", 
-                "actionURL": "https://www.dingtalk.com/"
-            }, 
-            {
-                "title": "不感兴趣", 
-                "actionURL": "https://www.dingtalk.com/"
-            }
-        ]
-    }
+     "title": "测试标题",
+     "text": "内容测试",
+     "singleTitle": "查看详情",
+     "singleURL": "https://open.dingtalk.com"
 }
 EOF;
 
