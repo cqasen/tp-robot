e6c59e92f365211403502f5cd04da3d6f9303613
cqasen <cqasen@qq.com>
2022-08-14 13:57:23 +0800

    11

--- app/service.php
+++ app/service.php
@@ -1,10 +1,10 @@
 <?php
 
 use app\AppService;
-
+use app\service\MiniProgramService;
 // 系统服务定义文件
 // 服务在完成全局初始化之后执行
 return [
 	AppService::class,
-    \app\service\MiniProgramService::class,
+    MiniProgramService::class,
 ];
