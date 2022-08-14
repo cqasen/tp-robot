b65ab43a2b13ffc4668e80f4038c71376cf2d8df
cqasen <cqasen@qq.com>
2022-08-14 15:20:12 +0800

    登录

--- app/controller/Auth.php
+++ app/controller/Auth.php
@@ -24,25 +24,25 @@ class Auth extends BaseController
      */
     public function login(Request $request)
     {
-        $code = $request->code;
-        $iv = $request->iv;
-        $encryptedData = $request->encryptedData;
+        $code = $request->post('code');
+        $iv = $request->post('iv');
+        $encryptedData = $request->post('encryptedData');
         /**
          * @var Application $miniProgram
          */
         $miniProgram = app('miniProgram');
         $wxInfo = $miniProgram->auth->session($code);
-        $decryptedData = [];
-        if (isset($wxInfo['errcode']) && $wxInfo['errcode'] != 0) {
+//        $decryptedData = [];
+//        if (isset($wxInfo['errcode']) && $wxInfo['errcode'] != 0) {
+//
+//        } else {
+//            /**
+//             * {"openId":"oCkbm5HU63Kcdx9xJbegC-4TWRbA","nickName":"森","gender":1,"language":"zh_CN","city":"北碚","province":"重庆","country":"中国","avatarUrl":"https://thirdwx.qlogo.cn/mmopen/vi_32/MJdzHRsaDY5WaL0n5YC0hpJxWOJTLcfkr7RRLWO9LCS2RyOkAGzDKres7Jx9IQJpIhR9MYh8kEZzIXibOJ1JEmA/132","watermark":{"timestamp":1611498927,"appid":"wxb85794a35a0430b4"}}
+//             */
+//            $decryptedData = $miniProgram->encryptor->decryptData($wxInfo['session_key'], $iv, $encryptedData);
+//        }
 
-        } else {
-            /**
-             * {"openId":"oCkbm5HU63Kcdx9xJbegC-4TWRbA","nickName":"森","gender":1,"language":"zh_CN","city":"北碚","province":"重庆","country":"中国","avatarUrl":"https://thirdwx.qlogo.cn/mmopen/vi_32/MJdzHRsaDY5WaL0n5YC0hpJxWOJTLcfkr7RRLWO9LCS2RyOkAGzDKres7Jx9IQJpIhR9MYh8kEZzIXibOJ1JEmA/132","watermark":{"timestamp":1611498927,"appid":"wxb85794a35a0430b4"}}
-             */
-            $decryptedData = $miniProgram->encryptor->decryptData($wxInfo['session_key'], $iv, $encryptedData);
-        }
-
-        return $this->responseJson($decryptedData);
+        return $this->responseJson($wxInfo);
     }
 
 }
